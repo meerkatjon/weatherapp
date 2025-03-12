25f6b2d7f30aef8f7781abf724c2cd7df6e792d4
usmail2paul <usmail2paul@gmail.com>
2025-03-12 19:20:47 -0400

    App initial build

--- src/main/java/model/WeatherResponse.java
+++ src/main/java/model/WeatherResponse.java
@@ -1,5 +1,47 @@
 package model;
 
+import model.WeatherResponse.Main;
+
 public class WeatherResponse {
+	
+	
+	private Main main;
+	private String name;
+	
+	
+	public Main getMain() {
+		return main;
+	}
+
+	public void setMain(Main main) {
+		this.main = main;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public static class Main {
+		
+		private double temp;
+		private int humidity;
+		
+		public double getTemp() {
+			return temp;
+		}
+		public void setTemp(double temp) {
+			this.temp = temp;
+		}
+		public int getHumidity() {
+			return humidity;
+		}
+		public void setHumidity(int humidity) {
+			this.humidity = humidity;
+		}
+	}
 
 }
